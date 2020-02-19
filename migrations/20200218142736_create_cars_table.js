2a4be6dd71ae1b30c0b26b97d9947d1bc818fb6e
[Arin Ramer] <[arinramer@gmail.com]>
2020-02-18 16:38:07 -0800

    Fixed a thing

--- migrations/20200218142736_create_cars_table.js
+++ migrations/20200218142736_create_cars_table.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('cars', tbl => {
-      tbl.increments().notNullable().index();
-      tbl.string('VIN').notNullable();
+      tbl.increments().notNullable();
+      tbl.string('VIN').notNullable().index();
       tbl.string('Make').notNullable();
       tbl.string('Model').notNullable();
       tbl.integer('Milage').notNullable();
